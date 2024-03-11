e3f7b4220783e2f043430793c086673694844bd8
sunghwki <swkim12345@naver.com>
2024-03-11 13:00:16 +0900

    add die flag

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: sunghwki <sunghwki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 15:27:26 by sunghwki          #+#    #+#             */
-/*   Updated: 2024/03/11 10:51:13 by sunghwki         ###   ########.fr       */
+/*   Updated: 2024/03/11 12:58:28 by sunghwki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ typedef struct s_thread {
 	long	*count_eat;
 	long	*start_time; //start time을 parent에서 정해준다.
 	long	*how_many_eat;
-	int		*flag;
+	int		*flag; //die flag를 설정해준다.
 } t_thread;
 
 int	ft_islong(const char *str);
